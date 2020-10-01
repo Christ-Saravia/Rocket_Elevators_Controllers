32306fe67c7ac03b09fcb915dd1ea8b6c524d334
Christ-Saravia <71100571+Christ-Saravia@users.noreply.github.com>
2020-10-01 07:59:02 -0400

    Add files via upload
    update du main.py : debut de requestElevator

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-
+num_floor = 10
+elevator_number = 2
+positionAscenseurA = 2
+positionAscenseurB = 6
+minimumFloor = 1
+maximumFloor = num_floor
+status = 0
+floorWhereRequestWasMade = 1
 
 
 
@@ -21,14 +28,14 @@ class Column:
         for x in elevatorList:
             print(x)
 
-# creation de button haut et bas pour chaque etage 
+# creation de button haut et bas pour chaque etage a l'exterieur des ascenseurs
 
     def createButtonList(self):
         buttonList = ['UP'] * self.num_floor , ['DOWN'] * self.num_floor
-        buttonList[self.elevator_number - self.elevator_number][self.minimumFloor - 1] = 'DISABLE'
-        buttonList[self.elevator_number - 1][self.maximumFloor - 1] = 'DISABLE'
-        for w in buttonList:
-            print(w)
+        buttonList[self.elevator_number - self.elevator_number][self.maximumFloor - 1] = 'DISABLE'
+        buttonList[self.elevator_number - 1][self.minimumFloor - 1] = 'DISABLE'
+        for externView in buttonList:
+            print(externView)  
 
 #Sa cest ce que l'utilisateur vont voir a l'interieur de l'ascenseur
 
@@ -38,7 +45,9 @@ class Column:
         while iterator > 0:
             panelList[iterator - 1] = iterator
             iterator -= 1
+        print('Voici panel list: ')
         print(panelList)
+
 
 #
 class Elevator:
@@ -50,6 +59,7 @@ class Elevator:
     def createDoorsList(self):
         doorsList = ['CLOSED'] * self.num_floor
         doorsList[self.status] = ux
+        print('voici etat des portes par etages vue de lexterieur')
         print(doorsList)
 
 
@@ -59,16 +69,29 @@ class Doors:
         self.status = status
     
     def doorsControl(self):
-        return 'OPENED'
+        if self.status == 1:
+            doorsStatus = 'OPENED'
+        else:
+            doorsStatus = 'CLOSED'
+        return doorsStatus
 
 #appel un ascenseur ou signaler qu'un etage demande un ascenseur 
 class Button:
-    def __init__(self, num_floor):
+    def __init__(self, num_floor,floorWhereRequestWasMade, status):
         self.floor = num_floor
+        self.signal = floorWhereRequestWasMade
+        self.status = status
     
-    def createAppelButtonList(self):
+    def RequestElevator(self):
         appelButtonList = ['OFF'] * self.floor
+        if self.status == 1:
+            appelButtonList[self.signal] = 'ON'
         print(appelButtonList)
+        return True
+
+#pour le moment je le fais comme une class, mais va etre une fonction que je vais mettre a l'interieur d'une class
+#class GoTo:
+
 
 
 f = Column(10, 2, 2, 6, 1, 10)
@@ -76,42 +99,51 @@ f.createElevatorList()
 f.createPanelList()
 f.createButtonList()
 
-y = Column(num_floor, elevator_number, positionAscenseurA, positionAscenseurB, minimumFloor, maximumFloor)
-y.createElevatorList()
-
-j = y
-j.createPanelList()
-
-w = y
-w.createButtonList()
-
-u = Doors(num_floor)
+u = Doors(status)
 ux = u.doorsControl()
+
 
 o = Elevator(num_floor, ux, status)
 o.createDoorsList()
 
-k = Button(num_floor)
-k.createAppelButtonList()
+k = Button(num_floor,floorWhereRequestWasMade,status)
+k.RequestElevator()
 
 
+"""
+ JE VAIS FAIRE DES TESTS
+ SCENARIO A
+ 
+ ELEVATOR A EST INACTIF AU 2E ETAGE
+ ELEVATOR B EST INACTIF AU 6E ETAGE
+ Someone is on floor 3 and wants to go to the 7th floor. 
+ Elevator A is expected to be sent.
+"""
+print('SCENARIO A COMMENCE')
+
+scenarioA = Column(10,2,2,6,1,10)
+
+print('UP(u)')
+print('DOWN(d)')
+
+actionButton = input()
+if actionButton == 'u':
+    floorWhereRequestWasMade = 3
+    status = 1
+
+f = Column(10, 2, 2, 6, 1, 10)
+f.createElevatorList()
+f.createPanelList()
+f.createButtonList()
+
+u = Doors(status)
+ux = u.doorsControl()
 
 
+o = Elevator(num_floor, ux, status)
+o.createDoorsList()
 
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
+k = Button(num_floor,floorWhereRequestWasMade,status)
+k.RequestElevator()
 
 
