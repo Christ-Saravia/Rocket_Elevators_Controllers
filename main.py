259ff9f6f1734e11d4ae67b27d80ed5bbda2b0f5
Christ-Saravia <71100571+Christ-Saravia@users.noreply.github.com>
2020-10-03 00:00:15 -0400

    Add files via upload
    version final

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+from playsound import playsound
+
 num_floor = 10
 elevator_number = 2
 positionAscenseurA = 2
 positionAscenseurB = 6
 minimumFloor = 1
 maximumFloor = num_floor
-status = 1      #0 = false pour le moment et 1 = true  
-floorWhereRequestWasMade = 10
-
+status = 0      #0 = false pour le moment et 1 = true  
+floorWhereRequestWasMade = 3
+ 
 
 
 
@@ -26,8 +28,9 @@ class Column:
         elevatorList [0][elevator_number - 2] = self.positionAscenseurA
         elevatorList [1][elevator_number - 1] = self.elevator_number
         elevatorList [1][elevator_number - 2] = self.elevator_number - 1
-        for x in elevatorList:
-            print(x)
+        joe = elevatorList [1][elevator_number - 2]
+        martin = elevatorList [1][elevator_number - 1] 
+        return joe, martin
 
 # creation de button haut et bas pour chaque etage a l'exterieur des ascenseurs
 
@@ -52,35 +55,72 @@ class Column:
             iterator -= 1
         print('Voici panel list: ')
         print(panelList)
+        return panelList
+
+def createElevatorTrack(jersey,vicky):
+    elevatorTrackA = [None] * num_floor
+    elevatorTrackB = [None] * num_floor
+    elevatorTrackA[positionAscenseurA - 1] = jersey
+    elevatorTrackB[positionAscenseurB - 1] = vicky
+    return elevatorTrackA,elevatorTrackB
 
 
 #
 class Elevator:
-    def __init__(self, num_floor, ux, status, floorWhereRequestWasMade):
+    def __init__(self, num_floor, ux, status, floorWhereRequestWasMade, elevatorTrackA,elevatorTrackB, positionAscenseurA, positionAscenseurB):
         self.num_floor = num_floor
         self.ux = ux
         self.status = status
         self.requestWasMade = floorWhereRequestWasMade
-    
+        self.elevatorTrackA = elevatorTrackA
+        self.elevatorTrackB = elevatorTrackB
+        self.positionAscenseurA = positionAscenseurA
+        self.positionAscenseurB = positionAscenseurB
+
     def createDoorsList(self):
-        doorsList = ['CLOSED'] * self.num_floor
-        doorsList[self.status] = ux
-        print('voici etat des portes par etages vue de lexterieur')
-        print(doorsList)
+        doorsList = ['CLOSED'] * self.num_floor, ['CLOSED'] * self.num_floor
+        doorsList[self.status][self.status] = ux
+        print('\n Here is the state of the doors on your floor as seen from the outside presently\n')
+        doorTrackA = doorsList[0][self.requestWasMade - 1]
+        doorTrackB = doorsList[1][self.requestWasMade - 1]
+        return doorTrackA, doorTrackB
+
 
     def createMoveUp(self):
+        boundPickupA = (self.positionAscenseurA -1)-(floorWhereRequestWasMade - 1)
+        boundPickupB = (self.positionAscenseurB - 1)-(floorWhereRequestWasMade - 1)
+        if abs(boundPickupA) < abs(boundPickupB):
+            iterator = abs(boundPickupA)
+            while iterator > 0:
+                lastPositionElevatorA = self.elevatorTrackA.index(1)
+                print(self.elevatorTrackA)
+                playsound('timer.wav')
+                newPositionElevatorA = lastPositionElevatorA + 1
+                self.elevatorTrackA[lastPositionElevatorA] = None
+                self.elevatorTrackA[newPositionElevatorA] = 1
+                print(self.elevatorTrackA)
+                iterator -= 1
+                return self.elevatorTrackA.index(1)
+
+    def goToFloor(self):
         moveUp = 1
+        moveDown = -1 
         etageDesire = 8
-        difference = etageDesire - (floorWhereRequestWasMade - 1)
-        iterator = difference * moveUp
-        floorList = [None] * self.num_floor
-        floorList[self.requestWasMade -1] = 'A1'
-        print(floorList)
+        difference = (etageDesire - 1) - (floorWhereRequestWasMade - 1)
+        if difference > 0:
+            move = moveUp
+        else:
+            move = moveDown
+        iterator = difference * move
         while iterator > 0:
-            lastPositionElevator = floorList.index('A1')
-            floorList[lastPositionElevator] = None
-            floorList[lastPositionElevator + 1] = 'A1'
-            print(floorList)
+            playsound('exit_cue_y.wav')            
+            lastPositionElevator = self.elevatorTrackA.index(1)
+            self.elevatorTrackA[lastPositionElevator] = None
+            self.elevatorTrackA[lastPositionElevator + 1] = 1
+            print(self.elevatorTrackA)
+            if iterator == 1:
+                playsound('yay_z.wav')
+                print("Thank you for using Rocket Elevator!")
             iterator -= 1
             
 
@@ -99,22 +139,23 @@ class Doors:
 
 #appel un ascenseur ou signaler qu'un etage demande un ascenseur 
 class Button:
-    def __init__(self, num_floor,floorWhereRequestWasMade, status):
+    def __init__(self, num_floor,floorWhereRequestWasMade, status , direction):
         self.floor = num_floor
         self.signal = floorWhereRequestWasMade - 1
         self.status = status
+        self.direction = direction
+
     
-    def RequestElevator(self):
+    def requestElevator(self):
         appelButtonList = ['OFF'] * self.floor
         if self.status == 1:
             appelButtonList[self.signal] = 'ON'
-            iterator = 1 #verification si qq un a fait une demande dans les autres etages
-            while iterator > 0:
-                index = appelButtonList.index('ON')
-                iterator -= 1
-                print('The index of ON:', index)
-        print(appelButtonList)
-        return True
+        return appelButtonList
+    
+def searchElevator(arr, x):
+    for buddah, value in enumerate(arr):
+        if value == x:
+            return buddah + 1
 
 
 
@@ -124,24 +165,13 @@ class GoTo:
         self.nom = nom
     def commencerLeProgramme(self):
         print('Do you want to go up or down? \n')
-        print('<Up> = U <Down> = D \n')
-        actionButton = input()
+        print('<Up> = Enter the key U \n<Down> = Enter the key D \n')
+        actionButton = print('Enter your key:'), input()
         if actionButton == 'u' or 'U':
-            floorWhereRequestWasMade = 3
+            print("You decided to go upstairs\n")
+            floorWhereRequestWasMade = self.nom
             status = 1
-            return floorWhereRequestWasMade,status
-
-
-#pour le moment je le fais comme une class, mais va etre une fonction que je vais mettre a l'interieur d'une class
-class FindBestElevator:
-    def __init__(self, arr, x):
-        self.arr = arr
-        self.x = x
-    def search(self):
-        for index, value in enumerate(arr):
-            if value == x:
-                return index
-        return -1
+            return status, floorWhereRequestWasMade, actionButton
 
 """
  JE VAIS FAIRE DES TESTS
@@ -153,35 +183,69 @@ class FindBestElevator:
  Elevator A is expected to be sent.
 """
 print(
-    '\n SCENARIO A COMMENCE: \n Elevator A est au 2e etage \n Elevator B est au 6e etage \n Vous etes au 3e etages et vous voulez aller au 7e etages \n'
+    '\n SCENARIO A : \n-Elevator A is on the 2nd floor \n-Elevator B is on the 6th floor \n You are on the 3rd floor and you want to go to the 7th floor. \n'
     )
 
 scenarioA = Column(10,2,2,6,1,10,floorWhereRequestWasMade)
-
 scenarioA.createButtonList()
 
 
-g = GoTo(5)
-g.commencerLeProgramme()
+g = GoTo(3)
+monday = g.commencerLeProgramme()
+arrayID = scenarioA.createElevatorList()
+direction = monday[2]
 
-    
+k = Button(num_floor,floorWhereRequestWasMade,monday[0], direction)
+
+k.requestElevator()
+
+manon = createElevatorTrack(arrayID[0],arrayID[1])
+  
 u = Doors(status)
 ux = u.doorsControl()
 
 
-o = Elevator(num_floor, ux, status, floorWhereRequestWasMade)
-o.createDoorsList()
-
-k = Button(num_floor,floorWhereRequestWasMade,status)
-k.RequestElevator()
-
-
-o.createMoveUp()
-
-arr = [1,10,30,15]
-x = 10
+o = Elevator(num_floor, ux, status, floorWhereRequestWasMade, manon[0], manon[1],positionAscenseurA,positionAscenseurB)
+portes = o.createDoorsList()
+portesA = portes[0]
+portesB = portes[1]
+print("p1: ", portesA)
+print("p2: ", portesB)
 
 
-r = FindBestElevator(arr,x)
-print(x, "is present at index", r.search())
+
+johanne = o.createMoveUp()
+if johanne == floorWhereRequestWasMade - 1:
+    d = Doors(1)
+    ux = d.doorsControl()
+    changement = ux
+    o = Elevator(num_floor, ux, status, floorWhereRequestWasMade, manon[0], manon[1],positionAscenseurA,positionAscenseurB)
+    portes = o.createDoorsList()
+    portesA = portes[0]
+    portesA = changement
+    portesB = portes[1]
+    playsound('boxing_bell.wav') 
+    print("[ring! ring ! The elevator has just arrived at the request floor]\n ")
+    print("p1: ", portesA)
+    print("p2: ", portesB)
+    playsound('traffic_city.wav')
+    d = Doors(0)
+    ux = d.doorsControl()
+    changement = ux
+    o = Elevator(num_floor, ux, status, floorWhereRequestWasMade, manon[0], manon[1],positionAscenseurA,positionAscenseurB)
+    portes = o.createDoorsList()
+    portesA = portes[0]
+    portesA = changement
+    portesB = portes[1]
+    print("p1: ", portesA)
+    print("p2: ", portesB) 
+
+o.goToFloor()
+
+
+
+
+
+
+
 
