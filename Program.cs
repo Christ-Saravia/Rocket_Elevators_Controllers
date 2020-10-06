1cc281e54ecd5de0cd46b696c13a85066d90ab1f
Christ-Saravia <71100571+Christ-Saravia@users.noreply.github.com>
2020-10-06 16:14:50 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -21,13 +21,10 @@ namespace src
 
             for(int i = 0; i < colonne; i++){
                 columnList.SetValue("A", i);
-                Console.WriteLine( "[Colonne]:   {0}", columnList.GetValue( i ) );
-                //elevatorList.SetValue(1, i, i+1);
-               // Console.WriteLine("[1,3]: {0}", elevatorList.GetValue(i,i));
+                Console.WriteLine( "[Colonne]:   {0}", columnList.GetValue( i ));
                 for(int e = 0; e < cage; e++){
                     elevatorList.SetValue(e+1, i, e);
-                    Console.WriteLine("[Ascenseur]: {0}", columnList.GetValue(i,e));  
-                   
+                    Console.WriteLine("[Ascenseur]: {0}", elevatorList.GetValue(i,e));         
                 }            
             }
         }
